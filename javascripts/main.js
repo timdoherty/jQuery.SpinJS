92f8ea6a61d061fbc33e2b5c78dc7b3c65ec5872
tdoherty <tim@dohertycomputing.com>
2013-03-08 13:57:57 -0800

    first commit

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,39 @@
-console.log('This would be the main JS file.');
+//require.config({
+//  paths: {
+//    'jquery': 'jquery',
+//    'spin'
+//  }
+//});
+require(
+  [
+    'jquery',
+    'jquery.spin'
+  ], function ($) {
+
+    $(function () {
+      $('.spinDemo.one').spin({ bgColor: 'black', color: 'eee', opacity: 3 });
+      $('.spinDemo.two').spin({
+        lines: 13,
+        length: 0,
+        radius: 38,
+        bgColor: 'yellow',
+        color: 'purple',
+        opacity: 4,
+        speed: 0.8,
+        corners: 1.0
+      });
+      $('.spinDemo.three').spin({
+        lines: 17,
+        bgColor: 'blue',
+        color: 'green',
+        opacity: 7,
+        length: 30,
+        width: 6,
+        corners: 1.0,
+        trail: 62,
+        speed: 1.8,
+        hwaccel: true
+      });
+    });
+  }
+);
